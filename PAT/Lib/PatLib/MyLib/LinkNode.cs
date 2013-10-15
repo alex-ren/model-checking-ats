b200a8ce825d8ef72227e0688467e2bed390e3f2
Alex-Thinkpad <alex.ren2006@gmail.com>
2013-10-14 22:07:56 -0400

    add support for ref (not done)

---
+++ PAT/Lib/PatLib/MyLib/LinkNode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PAT.Common.Classes.Expressions.ExpressionClass;
+
+using PAT.Lib;
+
+namespace PAT.Lib
+{
+    public class LinkedNode : ExpressionValue
+    {
+        private Object m_v;
+        private LinkedNode m_next;
+        private Guid m_id;
+
+        public LinkedNode(Object v, LinkedNode next)
+        {
+            m_v = v;
+            m_next = next;
+            m_id = Guid.NewGuid();
+        }
+
+        public LinkedNode()
+        {
+        }
+
+        public Object getValue()
+        {
+            return m_v;
+        }
+
+        public LinkedNode getTail()
+        {
+            return m_next;
+        }
+
+        private string getContent()
+        {
+            string ret = "";
+            LinkedNode link = this;
+            while (null != link)
+            {
+                ret += link.m_v.ToString();
+                ret += "-";
+                link = link.m_next;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Please implement this method to provide the string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        ///        
+        public override string ToString()
+        {
+            return getContent();
+        }
+
+        /// <summary>
+        /// Please implement this method to return a deep clone of the current object
+        /// </summary>
+        /// <returns></returns>
+        public override ExpressionValue GetClone()
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// Please implement this method to provide the compact string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        public override string ExpressionID
+        {
+            get { return getContent(); }
+        }
+    }
+}
+
---
+++ PAT/Lib/PatLib/MyLib/LinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PAT.Common.Classes.Expressions.ExpressionClass;
+
+using PAT.Lib;
+
+namespace PAT.Lib
+{
+    public class LinkedList : ExpressionValue
+    {
+        public LinkedList()
+        {
+        }
+
+        public Object list_nil()
+        {
+            return null;
+        }
+
+        public LinkedNode list_cons(Object header, LinkedNode tail)
+        {
+            return new LinkedNode(header, tail);
+        }
+
+        public Boolean list_is_empty(LinkedNode xs)
+        {
+            return (null == xs);
+        }
+
+        public Object list_get_header(LinkedNode xs)
+        {
+            return xs.getValue();
+        }
+
+        public LinkedNode list_get_tail(LinkedNode xs)
+        {
+            return xs.getTail();
+        }
+
+        /// <summary>
+        /// Please implement this method to provide the string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        ///        
+        public override string ToString()
+        {
+            return "LinkedList";
+        }
+
+        /// <summary>
+        /// Please implement this method to return a deep clone of the current object
+        /// </summary>
+        /// <returns></returns>
+        public override ExpressionValue GetClone()
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// Please implement this method to provide the compact string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        public override string ExpressionID
+        {
+            get { return this.ToString(); }
+        }
+    }
+}
